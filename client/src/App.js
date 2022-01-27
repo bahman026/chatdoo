ef026146c227febd55599c3af853adf6f3d79456
bahman <bahman026@gmail.com>
2022-01-27 22:21:55 +0330

    install and config tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          hello chatdoo
-        </p>
+      <div>
+          <h1 className="text-3xl font-bold underline">
+              Hello world!
+          </h1>
+      </div>
       </header>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const element = (
+    <App/>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
