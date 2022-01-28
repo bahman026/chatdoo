1f0bb0bdd2363a00d43e45d6ee2e20ca13181805
bahman <bahman026@gmail.com>
2022-01-28 09:23:56 +0330

    some change

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+
 import React from "react";
 
+import telegram from './assets/images/telegram.svg';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </div>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
